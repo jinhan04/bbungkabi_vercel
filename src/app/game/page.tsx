ac48d39c20af9246ad869f8c1e19b1435818f0e9
임진한 <jinhan0418@gmail.com>
2025-05-20 14:22:41 +0900

    v1.7

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -1,5 +1,6 @@
 // game/page.tsx
 "use client";
+export const dynamic = "force-dynamic";
 
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";

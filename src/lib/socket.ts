cd84f6b5b968e32d3832d219c0c8b444e05c3ae9
임진한 <jinhan0418@gmail.com>
2025-05-21 14:46:13 +0900

    v1.25

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -28,7 +28,7 @@ let socket: Socket | null = null;
 // VERCEL
 export const getSocket = () => {
   if (!socket) {
-    socket = io("wss://bbungkabe.com", {
+    socket = io("https://api.bbungkabe.com", {
       autoConnect: false,
       transports: ["websocket"],
     });

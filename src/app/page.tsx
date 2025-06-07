23e636b3641a799c75977f8cde381370b299f73b
임진한 <jinhan0418@gmail.com>
2025-06-07 10:56:05 +0900

    v5.19

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -203,8 +203,9 @@ export default function HomePage() {
       )}
       {showEasterEgg && (
         <div className="fixed top-20 left-1/2 transform -translate-x-1/2 bg-yellow-200 border border-yellow-400 text-yellow-900 px-6 py-3 rounded-xl shadow-xl z-50 animate-bounce text-center text-lg font-bold">
-          🎉 진한이 숨겨둔 이스터에그, 당신이 찾았군..화면 캡쳐해서 보내면
-          기프티콘 드림
+          🎉 진한이 숨겨둔 이스터에그, 당신이 찾았군..
+          <br />
+          &nbsp;&nbsp;&nbsp;&nbsp;화면 캡쳐해서 보내면 기프티콘 드림
         </div>
       )}
 
